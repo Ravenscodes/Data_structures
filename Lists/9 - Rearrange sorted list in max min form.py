f1ee1d9c1c1098177a0e0e7285a1c636adb7bc76
Ryazanskiy.M <ryazanskiy.m@wildberries.ru>
2022-04-10 16:14:16 +0300

    9 - Rearrange sorted list in max min form

--- Lists/9 - Rearrange sorted list in max min form.py
+++ Lists/9 - Rearrange sorted list in max min form.py
@@ -32,5 +32,29 @@ def max_min(lst):
     return new_lst
 
 
+def max_min_without_extra_space(lst):
+    # Return empty list for empty list
+    if len(lst) is 0:
+        return []
+
+    maxIdx = len(lst) - 1  # max index
+    minIdx = 0  # first index
+    maxElem = lst[-1] + 1  # Max element
+    # traverse the list
+    for i in range(len(lst)):
+        # even number means max element to append
+        if i % 2 == 0:
+            lst[i] += (lst[maxIdx] % maxElem) * maxElem
+            maxIdx -= 1
+        # odd number means min number
+        else:
+            lst[i] += (lst[minIdx] % maxElem) * maxElem
+            minIdx += 1
+
+    for i in range(len(lst)):
+        lst[i] = lst[i] // maxElem
+    return lst
+
+
 print(max_min([1, 2, 3, 4, 5]))
 print(max_min([1, 2, 3, 4, 5, 6]))
