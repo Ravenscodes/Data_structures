6c07bcb86166c51bbde084a637ee5e6c831758fd
Ryazanskiy.M <ryazanskiy.m@wildberries.ru>
2022-04-10 16:13:25 +0300

    9 - Rearrange sorted list in max min form

---
+++ Lists/9 - Rearrange sorted list in max min form.py
@@ -0,0 +1,36 @@
+"""
+Implement a function called max_min(lst)
+which will re-arrange the elements of a sorted list such that the 0th index will have the largest number,
+the 1st index will have the smallest, and the 2nd index will have second-largest, and so on.
+In other words, all the even-numbered indices will have the largest numbers in the list in descending order
+and the odd-numbered indices will have the smallest numbers in ascending order.
+
+Input:
+A sorted list
+
+Output:
+A list with elements stored in max/min form
+
+Sample Input
+lst = [1,2,3,4,5]
+
+Sample Output
+lst = [5,1,4,2,3]
+"""
+
+
+def max_min(lst):
+    new_lst = []
+    start, finish = 0, len(lst)-1
+    while start < finish:
+        new_lst.append(lst[finish])
+        new_lst.append(lst[start])
+        start += 1
+        finish -= 1
+    if start == finish:
+        new_lst.append(lst[start])
+    return new_lst
+
+
+print(max_min([1, 2, 3, 4, 5]))
+print(max_min([1, 2, 3, 4, 5, 6]))
