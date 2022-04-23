e1ba96b5a13b061580043bdafbf2db2a3ff6144f
Ryazanskiy.M <ryazanskiy.m@wildberries.ru>
2022-04-23 11:55:29 +0300

    Stack and Queue: Reversing First k elements of queue

---
+++ 3 - Stack and Queues/1 - Reversing First k Elements of Queue.py
@@ -0,0 +1,53 @@
+"""
+Implement the function reverseK(queue, k) which takes a queue and a number “k” as input
+and reverses the first “k” elements of the queue.
+
+Output
+The queue with first “k” elements reversed. Remember to return the queue itself!
+
+In case the value of “k” is larger than the size of the queue, is smaller than 0, or if the queue is empty,
+simply return None instead.
+
+Sample Input
+Queue = [1,2,3,4,5,6,7,8,9,10], k = 5
+
+Sample Output
+Queue = [5,4,3,2,1,6,7,8,9,10]
+"""
+
+from Queue import MyQueue
+from Stack import MyStack
+
+
+def reverseK(queue, k):
+    tmp_queue = MyQueue()
+    stack = MyStack()
+    if k <= 0 or queue.is_empty() or queue.size() < k:
+        return None
+    for i in range(k):
+        stack.push(queue.dequeue())
+    while not stack.is_empty():
+        tmp_queue.enqueue(stack.pop())
+    while not queue.is_empty():
+        tmp_queue.enqueue(queue.dequeue())
+
+    return tmp_queue
+
+
+queue = MyQueue()
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+queue.enqueue(4)
+queue.enqueue(5)
+queue.enqueue(6)
+queue.enqueue(7)
+queue.enqueue(8)
+queue.enqueue(9)
+queue.enqueue(10)
+print("the queue before reversing:")
+print(queue.items)
+tmp_queue = reverseK(queue, 5)
+print("the queue after reversing:")
+print(tmp_queue.items)
+
